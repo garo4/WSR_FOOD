69cdf499a18fd97283cae2d27399e36e63b87386
garo4 <nik.sleptsov56@mail.ru>
2022-05-16 18:08:27 +0900

    first commit

---
+++ app/src/main/java/com/example/wsr_food/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.wsr_food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, OnboardingScreen.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, 2000);
+    }
+
+
+}
